ed8b57526d30e9d83cb538aa820a22f90a6c11d9
klu2300031947 <163754507+klu2300031947@users.noreply.github.com>
2025-01-19 16:15:34 +0530

    Create LandingPage.js

---
+++ LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';  // Make sure to import your CSS file
+
+function App() {
+  return (
+    <div className="landing-page">
+      <h1>Plant Shop</h1>
+      <p>Welcome to the Plant Shop, your destination for beautiful houseplants!</p>
+      <button className="get-started-btn">Get Started</button>
+    </div>
+  );
+}
+
+export default App;
